0daf5c2146ade810413fc340db44940bf6f087e4
danielcoker <danielcoker28@gmail.com>
2019-11-29 21:12:14 +0100

    feat: Add fields to model

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    long_url = models.CharField(
+        max_length=250,
+        verbose_name="Long URL"
+    )
+    url_id = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.long_url
+
+    
+    def short(self):
+        return 'http://myshort.codes/' + self.url_id + '/'
+    
+
+    def long(self):
+        return self.long_url
