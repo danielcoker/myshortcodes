9573b350fc1d443c5fd78415be332fe3d7f2b68f
danielcoker <danielcoker28@gmail.com>
2019-12-02 23:55:12 +0100

    feat: Redirect to long URL

--- urlshortener/url.py
+++ urlshortener/url.py
@@ -5,5 +5,7 @@ from . import views as url_views
 app_name = 'urlshortener'
 
 urlpatterns = [
+    path('unknown-url/', url_views.unknown_url, name='unknown_url'),
+    path('<url_id>/', url_views.get_url, name='get_url'),
     path('', url_views.index, name='index'),
 ]
--- urlshortener/views.py
+++ urlshortener/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UrlForm
 from django.contrib import messages
+from .models import Url
 
 
 def index(request):
@@ -17,3 +18,15 @@ def index(request):
         url_form = UrlForm()
         
     return render(request, 'urlshortener/home.html', {'url_form':url_form})
+
+
+def get_url(request, url_id):
+    try:
+        url_object = Url.objects.get(url_id=url_id)
+        return redirect(url_object.long())
+    except Url.DoesNotExist:
+        return redirect('urlshortener:unknown_url')
+
+
+def unknown_url(request):
+    return render(request, 'urlshortener/unknown_url.html')
